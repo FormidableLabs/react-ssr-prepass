4e512cbb290f3a9ee2e8860338faa88dd02f3d60
Phil Pluckthun <phil@kitten.sh>
2020-11-05 02:10:26 +0000

    Collapse flushFrames implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { type Node, type Element } from 'react'
+import { type Node, type Element } from 'react'
 import type { Visitor, YieldFrame, Frame, AbstractElement } from './types'
 import { visit, update, SHOULD_YIELD } from './visitor'
 import { getChildrenArray } from './element'
@@ -13,51 +13,42 @@ import {
   Dispatcher
 } from './internals'
 
-const {
-  ReactCurrentDispatcher
-} = (React: any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
-
-/** visitChildren walks all elements (depth-first) and while it walks the
+/** visit() walks all elements (depth-first) and while it walks the
     element tree some components will suspend and put a `Frame` onto
     the queue. Hence we recursively look at suspended components in
     this queue, wait for their promises to resolve, and continue
-    calling visitChildren on their children. */
-const updateWithFrame = (
-  frame: Frame,
-  queue: Frame[],
-  visitor: Visitor
-): Promise<void> => {
+    calling visit() on their children. */
+const flushFrames = (queue: Frame[], visitor: Visitor): Promise<void> => {
+  const frame = queue.shift()
+  if (!frame) {
+    return Promise.resolve()
+  }
+
   if (SHOULD_YIELD && frame.kind === 'frame.yield') {
     frame.thenable = new Promise((resolve, reject) => {
       setImmediate(resolve)
     })
   }
 
-  return Promise.resolve(frame.thenable)
-    .catch((error) => {
+  return Promise.resolve(frame.thenable).then(
+    () => {
+      update(frame, queue, visitor)
+      return flushFrames(queue, visitor)
+    },
+    (error: Error) => {
       if (!frame.errorFrame) throw error
       frame.errorFrame.error = error
       update(frame, queue, visitor)
-    })
-    .then(() => {
-      update(frame, queue, visitor)
-    })
-}
-
-const flushFrames = (queue: Frame[], visitor: Visitor): Promise<void> => {
-  const frame = queue.shift()
-  if (!frame) return Promise.resolve()
-
-  return updateWithFrame(frame, queue, visitor).then(() =>
-    flushFrames(queue, visitor)
+    }
   )
 }
 
 const defaultVisitor = () => undefined
 
 const renderPrepass = (element: Node, visitor?: Visitor): Promise<void> => {
+  if (!visitor) visitor = defaultVisitor
+
   const queue: Frame[] = []
-  const fn = visitor !== undefined ? visitor : defaultVisitor
 
   // Context state is kept globally and is modified in-place.
   // Before we start walking the element tree we need to reset
@@ -67,12 +58,12 @@ const renderPrepass = (element: Node, visitor?: Visitor): Promise<void> => {
   setCurrentErrorFrame(null)
 
   try {
-    visit(getChildrenArray(element), queue, fn)
+    visit(getChildrenArray(element), queue, visitor)
   } catch (error) {
     return Promise.reject(error)
   }
 
-  return flushFrames(queue, fn)
+  return flushFrames(queue, visitor)
 }
 
 export default renderPrepass
