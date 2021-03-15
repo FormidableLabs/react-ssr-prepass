9a5737e3a177ecc95c06b7477fb731ce86b0ef72
Phil Pluckthun <phil@kitten.sh>
2021-03-15 14:42:42 +0000

    Add script to move typings to dist output

---
+++ scripts/copy-typings.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+
+fs.copyFileSync(
+  path.resolve(__dirname, 'react-ssr-prepass.d.ts'),
+  path.resolve(__dirname, '../dist/react-ssr-prepass.d.ts')
+)
+
+fs.copyFileSync(
+  path.resolve(__dirname, 'react-ssr-prepass.js.flow'),
+  path.resolve(__dirname, '../dist/react-ssr-prepass.js.ts')
+)
+
+fs.copyFileSync(
+  path.resolve(__dirname, 'react-ssr-prepass.js.flow'),
+  path.resolve(__dirname, '../dist/react-ssr-prepass.es.js.flow')
+)
